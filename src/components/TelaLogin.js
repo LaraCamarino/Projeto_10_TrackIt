c8458a74f8562a72a89294011425586217e3c828
LaraCamarino <laracamarino@gmail.com>
2022-05-28 20:00:23 -0300

    Versão 2 - Tela de Login

--- src/components/TelaLogin.js
+++ src/components/TelaLogin.js
@@ -1,5 +1,118 @@
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import styled from "styled-components";
+
+import { ThreeDots } from 'react-loader-spinner';
+import Logo from "../assets/logo.png"
+
 export default function TelaLogin() {
+    const navigate = useNavigate();
+
+    const [loading, setLoading] = useState(false);
+    const [login, setLogin] = useState({
+        email: "",
+        senha: ""
+    });
+
+    function fazerLogin() {
+        const URL = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login";
+
+        if (login.email.length > 0 && login.email.length > 0) {
+            setLoading(true);
+            const promise = axios.post(URL, {
+                email: login.email,
+                password: login.senha
+            });
+            promise.then(response => {
+                console.log(response.data);
+                navigate("/hoje");
+            })
+            promise.catch(err => {
+                alert(err.response.data.message);
+                console.log(err.response);
+                setLogin({
+                    email: "",
+                    senha: ""
+                });
+                setLoading(false);
+            })
+        }
+        else { alert("Preencha todos os campos.") }
+    }
+
+    function montarFormulario() {
+        if (!loading) {
+            return (
+                <>
+                    <input type="email" placeholder="email" value={login.email} onChange={(e) => setLogin({ ...login, email: e.target.value })} ></input>
+                    <input type="password" placeholder="senha" value={login.senha} onChange={(e) => setLogin({ ...login, senha: e.target.value })}></input>
+                    <button onClick={fazerLogin}>Entrar</button>
+                </>
+            )
+        }
+        else {
+            return (
+                <>
+                    <input type="email" placeholder="email" value={login.email} onChange={(e) => setLogin({ ...login, email: e.target.value })} disabled={true} ></input>
+                    <input type="password" placeholder="senha" value={login.senha} onChange={(e) => setLogin({ ...login, senha: e.target.value })} disabled={true} ></input>
+                    <button onClick={fazerLogin} disabled={true} ><ThreeDots width={51} height={13} color="#FFFFFF" /></button>
+                </>
+            )
+        }
+    }
+
     return (
-        <h1>TelaLogin</h1>
+        <Container>
+            <img src={Logo} alt=""></img>
+
+            {montarFormulario()}
+
+            <StyledLink to="/cadastro">
+                Não tem uma conta? Cadastre-se!
+            </StyledLink>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    img {
+        margin-bottom: 35px;
+    }
+
+    input {
+        width: 303px;
+        height: 45px;
+        border-radius: 5px;
+        border: 1px solid #D4D4D4;   
+        margin-bottom: 5px;
+    }
+
+    &::placeholder {
+        color: #DBDBDB;
+        font-size: 20px;
+    }
+
+    button {
+        width: 303px;
+        height: 45px;
+        background-color: #52B6FF;
+        border-radius: 5px;
+        border: 0px;
+        font-size: 20px;
+        color: #FFFFFF;
+        margin-bottom: 25px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+`
+const StyledLink = styled(Link)`
+    color: #52B6FF;
+`
