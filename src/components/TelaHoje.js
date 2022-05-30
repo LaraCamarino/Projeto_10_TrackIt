34f80c1046e4c1dded2a9f00746048481908229b
LaraCamarino <laracamarino@gmail.com>
2022-05-30 07:47:24 -0300

    Versão 5 - Tela de Hoje

--- src/components/TelaHoje.js
+++ src/components/TelaHoje.js
@@ -1,26 +1,139 @@
-import axios from "axios";
-//import { Link } from "react-router-dom";
-import { useState, useContext, useEffect } from "react";
 import styled from "styled-components";
-
+import axios from "axios";
+import { useState, useContext, useEffect } from "react";
 import UserContext from "../contexts/UserContext";
+import dayjs from "dayjs";
+import "dayjs/locale/pt-br";
 
 import Topo from "./Topo";
 import Menu from "./Menu";
+import Check from "../assets/vector.png"
+
 
 export default function TelaHoje() {
-    
+    const { usuario, progresso, setProgresso, setPorcentagem } = useContext(UserContext);
+    const config = {
+        headers: {
+            "Authorization": `Bearer ${usuario.token}`
+        }
+    };
+
+    const [habitosHoje, setHabitosHoje] = useState([]);
+
+    useEffect(() => {
+        const promise = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/today", config);
+
+        promise.then((response) => {
+            setHabitosHoje(response.data);
+            console.log(response.data);
+            for(let i = 0; i < response.data.length; i++) {
+                if(response.data[i].done) {
+                    setProgresso([...progresso, response.data[i].id]);
+                }
+            }
+        });
+        promise.catch((err) => console.log(err.response));
+
+    }, []);
+
+    function HabitoHoje({ id, name, done, currentSequence, highestSequence }) {
+        const [atualIgualRecorde, setAtualIgualRecorde] = useState(false);
+
+
+        function marcarHabito(id) {
+            if (!done) {
+                const promise = axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${id}/check`, null, config)
+
+                promise.then(() => {
+                    recarregarHabitosHoje();
+                    setProgresso([...progresso, id]);
+                });
+                promise.catch((err) => console.log(err.response));
+            }
+            else {
+                const promise = axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${id}/uncheck`, null, config);
+
+                promise.then(() => {
+                    recarregarHabitosHoje();
+                    setProgresso(progresso.filter((item) => item !== id));
+                });
+                promise.catch((err) => console.log(err.response));
+            }
+        }
+        function recarregarHabitosHoje() {
+            const promise = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/today", config);
+
+            promise.then((response) => {
+                setHabitosHoje(response.data);
+            });
+            promise.catch((err) => console.log(err.response));
+        }
+        function conferirIgualdade() {
+            if (currentSequence === highestSequence && highestSequence !== 0 && done) {
+                setAtualIgualRecorde(true);
+            }
+        }
+
+        useEffect(() => conferirIgualdade(), []);
+
+        return (
+            <CaixaHabito>
+                <span>
+                    <h2>{name}</h2>
+                    <SequenciaAtual done={done}>Sequência atual:<p>{currentSequence} dias</p> </SequenciaAtual>
+                    <SeuRecorde recorde={atualIgualRecorde} >Seu recorde:<p>{highestSequence} dias</p> </SeuRecorde>
+                </span>
+                <CheckMark done={done} onClick={() => marcarHabito(id)}>
+                    <img src={Check} alt=""></img>
+                </CheckMark>
+            </CaixaHabito>
+        )
+    }
+
+
+    function ListarHabitosHoje() {
+        return (
+            <>
+                {
+                    habitosHoje.map((item, index) => <HabitoHoje key={index} id={item.id} name={item.name} done={item.done} currentSequence={item.currentSequence} highestSequence={item.highestSequence} />)
+
+                }
+            </>
+        )
+    }
+
+
+    function PorcentagemProgresso() {
+        let porcento = Math.round((progresso.length / habitosHoje.length) * 100);
+        setPorcentagem(porcento);
+
+        if (progresso.length === 0 || isNaN(porcento)) {
+            return (
+                <Porcento feito={false}>Nenhum hábito concluído ainda</Porcento>
+            )
+        }
+        return (
+            <Porcento feito={true}>{porcento}% dos hábitos concluídos</Porcento>
+        )
+    }
+
+    const hoje = dayjs().locale("pt-br");
+    const diaDaSemana = hoje.format("dddd").charAt(0).toUpperCase() + hoje.format("dddd").slice(1);
+    const diaDoMes = hoje.format("DD/MM");
+
     return (
         <>
             <Topo />
             <Container>
 
                 <CaixaDia>
-                    <h1>Dia da Semana</h1>
-                    <p>Nenhum hábito concluído ainda</p>
+                    <h1>{diaDaSemana}, {diaDoMes}</h1>
+                    <PorcentagemProgresso />
                 </CaixaDia>
 
-                </Container>
+                <ListarHabitosHoje />
+
+            </Container>
             <Menu />
         </>
     )
@@ -35,16 +148,16 @@ const Container = styled.div`
 
 const CaixaDia = styled.div`
     margin-bottom: 28px;
+
     h1 {
         color: #126BA5;
         font-size: 23px;
         margin-bottom: 5px;
     }
-    p {
-        color: #BABABA;
-        font-size: 18px;
-        /* quando selecionado tiver mais que 0 habitos, deve ficar #8FC549 */
-    }
+`
+const Porcento = styled.p`
+    color: ${props => props.feito ? "#8FC549" : "#BABABA"};
+    font-size: 18px;
 `
 
 const CaixaHabito = styled.div`
@@ -56,14 +169,33 @@ const CaixaHabito = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+
     h2 {
         color: #666666;
         font-size: 20px;
         margin-bottom: 7px;
     }
+`
+
+const SequenciaAtual = styled.div`
+    display: flex;
+    flex-direction: row;
+    font-size: 12px;
+    color: #666666;
+
     p {
-        color: #666666;
-        font-size: 12px;
+        color: ${props => props.done ? "#8FC549" : "#666666"};;
+    }
+`
+
+const SeuRecorde = styled.div`
+    display: flex;
+    flex-direction: row;
+    font-size: 12px;
+    color: #666666;
+
+    p {
+        color: ${props => props.recorde ? "#8FC549" : "#666666"};;
     }
 `
 
@@ -71,11 +203,11 @@ const CheckMark = styled.div`
     width: 69px;
     height: 69px;
     border-radius: 5px;
-    background-color: #EBEBEB;
-    /* quando selecionado bkc #8FC549 */
+    background-color: ${props => props.done ? "#8FC549" : "#EBEBEB"};
     display: flex;
     justify-content: center;
     align-items: center;
+
     img {
         width: 35px;
         height: 28px;
